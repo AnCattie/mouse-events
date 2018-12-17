fc69a3b45cf553c1acd1521a8100fa182644f49b
An <an.arletta.cattie@gmail.com>
2018-12-17 12:25:00 +0100

    mouse events done

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+//Oefening 1 
+let hoverMe = function(event){
+    event.target.style.display = "none"; // doet de div bij hoverMe weg
+}
+let div1 = document.getElementsByClassName("hoverMe")[0];
+let div2 = document.getElementsByClassName("hoverMe")[1];
+let div3 = document.getElementsByClassName("hoverMe")[2];
+div1.addEventListener("mouseover", hoverMe);
+div2.addEventListener("mouseover", hoverMe);
+div3.addEventListener("mouseover", hoverMe);
 
+// Oefening 2
+let appear = function(event){
+    div1.style.display= "block";
+    div2.style.display= "block";
+    div3.style.display= "block";
+};
+
+let resetdiv = document.getElementById("reset");
+resetdiv.addEventListener("click", appear);
+
+// Oefening 3
+
+
+function showCoords(event) {
+    let x = event.clientX;
+    let y = event.clientY;
+    document.getElementById("x-axis").innerHTML = x;
+    document.getElementById("y-axis").innerHTML = y;
+    }
+    let bodyRef = document.body;
+    bodyRef.addEventListener("mousemove", showCoords);
