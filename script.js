7739d0fe4faab5620f867d6d7067f543db286a1f
An <an.arletta.cattie@gmail.com>
2018-12-17 16:02:54 +0100

    no message

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 //Oefening 1 
-let hoverMe = function(event){
-    event.target.style.display = "none"; // doet de div bij hoverMe weg (functie)
+
+function onzichtbaar(x){ // in html element al geselecteerd adhv (onmouseover - zichtbaar voor THIS div) - deze functie doorgegeven. x = hoverMe classes
+    x.style.visibility = "hidden"; // classes verbergen als er met de muis over wordt gegaan.
 }
-let div1 = document.getElementsByClassName("hoverMe")[0]; // selecteert & geeft variabel aan de 1ste class hoverMe
-let div2 = document.getElementsByClassName("hoverMe")[1]; // selecteert & geeft variabel aan de 2e class hoverMe
-let div3 = document.getElementsByClassName("hoverMe")[2]; // selecteert & geeft variabel aan de 3e class hoverMe
-div1.addEventListener("mouseover", hoverMe); // Geeft een functie aan de eerste div --> als er met de muis over gegaan wordt, start de functie style display none 
-div2.addEventListener("mouseover", hoverMe); // Geeft een functie aan de tweede div --> als er met de muis over gegaan wordt, start de functie style display none
-div3.addEventListener("mouseover", hoverMe); // Geeft een functie aan de derde div --> als er met de muis over gegaan wordt, start de functie style display none
 
 // Oefening 2
-let appear = function(event){
-    div1.style.display= "block"; // plaatst deze terug en blokkeert deze zodat ze blijven staan
-    div2.style.display= "block"; // plaatst deze terug en blokkeert deze zodat ze blijven staan
-    div3.style.display= "block"; // plaatst deze terug en blokkeert deze zodat ze blijven staan
-};
-let resetdiv = document.getElementById("reset"); // selecteerd de ID vanuit HMTL
-resetdiv.addEventListener("click", appear); // wanneer je klikt op de button - zal de 1ste - 2e & 3e div terug verschijnen
+let oefening1 = document.getElementsByClassName("hoverMe"); // divs geselecteerd van oefening 1
+function zichtbaar(){ // functie toewijzen voor zichtbaar te maken
+    for (i=0; i < oefening1.length; i++){ // i is 0, als i kleiner dan de lengte van oefenign 1 = dan is het i +1
+        oefening1[i].style.visibility = "visible"; // oefening 1 (i) wordt zichtbaar bij het klikken op de button
+    }
+}
 
 // Oefening 3
 function showCoords(event) { // functie om de coördinaten te tonen
